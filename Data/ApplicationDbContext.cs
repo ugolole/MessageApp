62924670de645c468755d378953c85a52abe9a37
ugolole <ugololeivan@yahoo.com>
2019-06-11 23:29:43 -0400

    adding entity frame work with complete application db context code

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Message.Data;
+
+namespace Message.Data{
+    public class ApplicationDbContext : DbContext{
+        
+        #region Constructor
+        public ApplicationDbContext(DbContextOptions options) : base(options){
+
+        }
+        #endregion Constructor
+
+        #region Methods
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Message>().ToTable("Message");
+            modelBuilder.Entity<Message>().Property(i => i.Id).ValueGeneratedOnAdd();
+
+            //no other relationship are present
+        }
+        #endregion Methods
+
+        #region Properties
+        //get and set messages.
+        public DbSet<Message> Messages {get; set;}
+
+        #endregion Properties
+    }
+}
