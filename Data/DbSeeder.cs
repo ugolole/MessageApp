724d0fd429e69124bdfcb20cecb4b5af71cb8557
Ivan Richard <ugoivan@gmail.com>
2019-06-13 08:06:56 -0400

    Testing push functionality with comment to the code

--- Data/DbSeeder.cs
+++ Data/DbSeeder.cs
@@ -4,10 +4,13 @@ using System.Threading.Tasks;
 using System.Linq;
 
 namespace Message.Data{
+    //Automatically adding data to the database using seeder class.
     public class DbSeeder{
 
         #region Constructor
         public DbSeeder(){}
         #endregion Constructor
+
+
     }
 }
