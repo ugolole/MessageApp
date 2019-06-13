6071789bd27be9f226399470a7401f0cae6f405c
Ivan Richard <ugoivan@gmail.com>
2019-06-13 08:08:07 -0400

    Revert "Testing push functionality with comment to the code"
    This reverts commit 724d0fd429e69124bdfcb20cecb4b5af71cb8557.

--- Data/DbSeeder.cs
+++ Data/DbSeeder.cs
@@ -4,13 +4,10 @@ using System.Threading.Tasks;
 using System.Linq;
 
 namespace Message.Data{
-    //Automatically adding data to the database using seeder class.
     public class DbSeeder{
 
         #region Constructor
         public DbSeeder(){}
         #endregion Constructor
-
-
     }
 }
